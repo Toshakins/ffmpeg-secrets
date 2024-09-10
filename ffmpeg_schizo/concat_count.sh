b2b90b92719fbab249294f298d177da95b1d7735
Anton Ovinnikov <toshakins@gmail.com>
2024-09-10 17:33:21 +0300

    Delete ffmpeg_schizo directory

--- ffmpeg_schizo/concat_count.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-grep duration concat.txt | awk '{ sum += $2 } END { print sum }'
--- ffmpeg_schizo/ffconcat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-ffmpeg -hide_banner -f concat -i concat.txt -fps_mode vfr -pix_fmt yuv420p -c:v libx264 output.mp4 -y &>/dev/null
-ffprobe -hide_banner -show_streams output.mp4 | grep --color "duration="
